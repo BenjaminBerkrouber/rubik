c9035148a06fb952113463e595da89e35d307eb6
Berkrouber Benjamin <benjamin.berkrouber@gmail.com>
2025-08-18 15:39:24 +0200

    feat(IEngine) : adding bridge function for set solutions

--- include/engine/IEngine.hpp
+++ include/engine/IEngine.hpp
@@ -3,8 +3,19 @@
 class IEngine {
     protected:
         CubeState _state;
+        std::vector<SpinLst> _solutionSpins;
+        std::vector<<std::pair<std::string, std::vector<SpinLst>> _solutionSteps;
 
     public:
         virtual void print() const = 0;
         virtual ~IEngine() = default;
+        
+        void setSolutionSpins(const std::vector<SpinLst>& solutionSpins) {
+            _solutionSpins = solutionSpins;
+        }
+
+        void setSolutionSteps(const std::vector<std::pair<std::string, std::vector<SpinLst>>>& solutionSteps) {
+            _solutionSteps = solutionSteps;
+        }
+
 };
